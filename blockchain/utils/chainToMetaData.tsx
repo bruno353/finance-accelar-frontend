08aa68ddd9f9bea13be274f19af0dae304ae8120
Bruno Laureano <brunolsantos152@gmail.com>
2024-08-27 15:51:33 -0300

    feat: changing crossfi network

--- blockchain/utils/chainToMetaData.tsx
+++ blockchain/utils/chainToMetaData.tsx
@@ -14,7 +14,7 @@ export const chainToCopy = {
     explore: 'https://scan.testnet.ms',
     name: 'Crossfi',
     currency: 'XFI',
-    contractAddress: '0xB8787cD673857529201c10B950d0b731255eCBBF',
+    contractAddress: '0xe2A4F6Cae191e6e599488E1e2C95861312Df9826',
     depinDplEstPrice: '~ XFI 4.6',
     chainId: 1115,
   },
